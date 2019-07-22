a26b358f0aa6798a68955d71158117d78930ebe7
Kit Kemper <cgkemper@gmail.com>
2019-07-22 11:28:08 -0400

    Added chapter 8.  Converted parent directory into an R project.

---
+++ chapter_8-workflows-projects.R
@@ -0,0 +1,15 @@
+# Chapter 8 - Workflows: Projects
+
+## 8.1 - What is real
+# Don't restore or save workspaces to assure that all of teh data is stored within the scripts of your projects.
+# Cmd + Shift + F10 restarts RStudio
+# Cmd = Shift + S will rerun the current script.
+
+## 8.2 - Where Does Your Analysis Live
+getwd()
+setwd("dropbox/sandbox/r4ds")
+getwd()
+
+## 8.3 - Paths and Directories
+
+## 8.4 - Rstudio Projects
