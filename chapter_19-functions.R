3245e846a3b83545f81cf1c66bc06fb7d9262811
Kit Kemper <cgkemper@gmail.com>
2019-10-24 15:41:46 -0400

    Through practice part 4 of chapter 19

--- chapter_19-functions.R
+++ chapter_19-functions.R
@@ -25,4 +25,48 @@ rescale01(c(Inf,  -Inf, 0:5, NA))
 
 #### 3.
 
+##### a.
+prop_na <- function(x) {
+  mean(is.na(x))
+}
+prop_na(c(0, 1, 2, NA, 4, NA))
 
+##### b.
+sum_to_one <- function(x) {
+  x / sum(x, na.rm = T)
+}
+
+sum_to_one(c(1, 2, 3, 4, 5))
+
+##### c.
+coef_variation <- function(x) {
+  sd(x, na.rm = T) / mean(x, na.rm = T)
+}
+
+coef_variation(c(1:5))
+
+#### 4.
+
+##### a.
+variance <- function(x, na.rm = T) {
+  n <- length(x)
+  m <- mean(x, na.rm = T)
+  sq_err <- (x-m)^2
+  sum(sq_err) / (n - 1)
+}
+
+var(1:10)
+
+variance(1:10)
+
+##### b.
+skewness <- function(x, na.rm = T){
+  n <- length(x)
+  m <- mean(x, na.rm = na.rm)
+  v <- var(x, na.rm = na.rm)
+  (sum((x - m)^3) / (n - 2)) / v^(3/2)
+}
+
+skewness(c(1,2, 5, 100))
+
+#### 5.
