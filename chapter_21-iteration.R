98999ae623ebef2752eeba695e6d7418e4d7931b
Kit Kemper <cgkemper@gmail.com>
2019-11-07 15:31:45 -0500

    through 21.8

--- chapter_21-iteration.R
+++ chapter_21-iteration.R
@@ -445,3 +445,92 @@ flatten_dbl(map(-2:2, rnorm, n = 5))
 x <- split(mtcars, mtcars$cyl)
 map(x, function(df) lm(mpg ~ wt, data = df))
 map(x, ~ lm(mpg ~ wt, data = .))
+
+## 21.6
+
+safe_log <- safely(log)
+str(safe_log(10))
+
+x <- list(1, 10, "a")
+y <- x %>% map(safely(log))
+str(y)
+
+y <- y%>% transpose()
+str(y)
+
+is_ok <- y$error %>% map_lgl(is_null)
+x[!is_ok]
+y$result[is_ok] %>% flatten_dbl()
+
+x <- list(1, 10, "a")
+x %>% map_dbl(possibly(log, NA_real_))
+
+x <- list(1, -1)
+x %>% map(quietly(log)) %>% str()
+
+## 21.7 - Mapping Over Multiple Arguments
+
+mu <- list(5, 10, -3)
+mu %>%
+  map(rnorm, n = 5) %>%
+      str()
+
+sigma <- list(1, 5, 10)
+seq_along(mu) %>%
+  map(~rnorm(5, mu[[.]], sigma[[.]])) %>%
+        str()
+
+map2(mu, sigma, rnorm, n = 5) %>% str()
+
+n <- list(1, 3, 5)
+args1 <- list(n, mu, sigma)
+args1 %>%
+  pmap(rnorm) %>%
+  str()
+
+args2 <- list(mean = mu, sd = sigma, n = n)
+args2 %>%
+  pmap(rnorm) %>%
+  str()
+
+params <- tribble(
+  ~mean, ~sd, ~n,
+  5, 1, 1,
+  10, 5, 3,
+  -3, 10, 5
+)
+
+params %>%
+  pmap(rnorm)
+
+f <- c("runif", "rnorm", "rpois")
+param <- list(
+  list(min = -1, max = 1),
+  list(sd = 1),
+  list(lambda = 10)
+)
+
+invoke_map(f, param, n = 5) %>% str()
+
+sim <- tribble(
+  ~f, ~params,
+  "runif", list(min = -1, max = 1),
+  "rnorm", list(sd = 5),
+  "rpois", list(lambda = 10)
+)
+sim %>%
+  mutate(sim = invoke_map(f, params, n = 10))
+
+## 21.8 - Walk
+
+x <- list(1, "a", 3)
+
+x %>%
+  walk(print)
+
+library(ggplot2)
+plots <- mtcars %>%
+  split(.$cyl) %>%
+  map(~ggplot(., aes(mpg, wt)) + geom_point())
+paths <- stringr::str_c(names(plots), ".pdf")
+pwalk(list(paths, plots), ggsave, path = tempdir())
