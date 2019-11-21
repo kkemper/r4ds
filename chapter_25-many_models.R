6799c9e1cf4925d3869c3698798b5ec11519fe5a
Kit Kemper <cgkemper@gmail.com>
2019-11-21 10:07:52 -0500

    Through 25.4.3.

--- chapter_25-many_models.R
+++ chapter_25-many_models.R
@@ -126,3 +126,113 @@ by_country %>%
   unnest(glance, .drop = T) %>%
   ggplot(aes(continent, r.squared)) +
   geom_jitter(width = 0.5)
+
+
+#### 2.
+library("ggbeeswarm")
+by_country %>%
+  mutate(glance = map(model, broom::glance)) %>%
+  unnest(glance, .drop = T) %>%
+  ggplot(aes(continent, r.squared)) +
+  geom_beeswarm()
+
+#### 3.
+
+gapminder %>%
+  group_by(country, continent) %>%
+  nest() %>%
+  mutate(model = map(data, ~ lm(lifeExp ~ year, .))) %>%
+  mutate(glance = map(model, broom::glance)) %>%
+  unnest(glance) %>%
+  unnest(data) %>%
+  filter(r.squared < 0.25) %>%
+  ggplot(aes(year, lifeExp)) +
+  geom_line(aes(color = country))
+
+## 25.3 List-Columns
+
+data.frame(x = list(1:3, 3:5))
+data.frame(
+  x = I(list(1:3, 3:5)),
+  y = c("1, 2", "3, 4, 5")
+)
+
+tibble(
+  x = list(1:3, 3:5),
+  y = c("1, 2", "3, 4, 5")
+)
+
+tribble(
+  ~x, ~y,
+  1:3, "1, 2",
+  3:5, "3, 4, 5"
+)
+
+## 25.4 - Creating List-Columns
+
+### 25.4.1 - Creating List-Columns with Nesting
+
+gapminder %>%
+  group_by(country, continent) %>%
+  nest()
+
+gapminder %>%
+  nest(year:gdpPercap)
+
+### 25.4.2 - Creating List-Columns from Vectorized Functions
+
+df <- tribble(
+  ~x1,
+  "a, b, c",
+  "d, e, f, g"
+)
+
+df  %>% mutate(x2 = stringr::str_split(x1, ","))
+df %>%
+  mutate(x2 = stringr::str_split(x1, ",")) %>%
+  unnest()
+
+sim <- tribble(
+  ~f, ~params,
+  "runif", list(min = -1, max =  1),
+  "rnorm", list(sd = 5),
+  "rpois", list(lambda = 10)
+)
+sim %>%
+  mutate(sims = invoke_map(f, params, n = 10))
+
+### 25.4.3 - Creating List-Columns from Multivalued Summaries
+
+mtcars %>%
+  group_by(cyl) %>%
+  summarize(quantile(mpg))
+mtcars %>%
+  group_by(cyl) %>%
+  summarize(q = list(quantile(mpg)))
+probs <- c(0.01, 0.25, 0.5, 0.75, 0.99)
+mtcars %>%
+  group_by(cyl) %>%
+  summarize(p = list(probs), q = list(quantile(mpg, probs))) %>%
+  unnest()
+
+### 25.4.3 - Creating List-Columns from a Named List
+
+x <- list(
+  a = 1:5,
+  b = 3:4,
+  c = 5:6
+)
+
+df <- enframe(x)
+df
+
+df %>%
+  mutate(smry = map2_chr(name, value, ~ stringr::str_c(.x, ": ", .y[1])))
+
+str_split(sentences[1:3], " ")
+str_match_all(c("abc", "aa", "aabaa", "abbbc"), "a+")
+map(1:3, runif)
+
+range(mtcars$mpg)
+fivenum(mtcars$mpg)
+boxplot.stats(mtcars$mpg)
